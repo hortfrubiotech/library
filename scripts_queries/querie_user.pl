a312dddb84702d0c0796cc2e303513b0155a5528
hugo-pH <hugoycia@gmail.com>
2013-11-06 11:54:31 +0100

    Update querie_user.pl

--- scripts_queries/querie_user.pl
+++ scripts_queries/querie_user.pl
@@ -8,10 +8,11 @@ while (my $line = <>) {
 
  my $mail = $line;
 
-my $ds = "DBI:mysql:biblioteca:localhost";
-my $user_sql = "hugo";
-my $passwd = "pimiento+34";
-my $dbh = DBI->connect($ds,$user_sql,$passwd) || die "Can't connect!"; 
+my $ds = "DBI:mysql:library:localhost";
+my $userdb;
+my $password;
+my $dbh = DBI->connect($ds. ";mysql_read_default_file=/home/.my.cnf",
+$userdb, $password) || die "Can't connect!";
 
 my $sth_user_id = $dbh->prepare("SELECT user_id FROM usuario WHERE mail LIKE ?");
 $sth_user_id->execute ($mail);
